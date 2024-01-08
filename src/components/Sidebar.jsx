6408265664c1c8ca83f1a4f0b81bb4579b43a971
ritiksharmarj <theonlyritik@gmail.com>
2024-01-09 03:26:37 +0530

    navbar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,11 +1,12 @@
 import { Link } from 'react-router-dom';
 import { ChevronDown } from '../assets/icons/ChevronDown';
 import Navbar from './Navbar';
+import { WalletIcon } from '../assets/icons/WalletIcon';
 
 export default function Sidebar() {
   return (
-    <div className='row-span-full bg-navbar-secondary flex flex-col py-4 px-[10px] gap-4 text-black-100'>
-      <div className='flex flex-col gap-6 items-center'>
+    <div className='row-span-full bg-navbar-secondary flex flex-col items-center py-4 px-[10px] gap-4 text-black-100'>
+      <div className='flex flex-col gap-6 items-center w-full grow'>
         {/* company */}
         <div className='flex gap-3 items-center w-[192px]'>
           <div className='w-10 h-10 rounded bg-black-100 overflow-hidden'>
@@ -31,7 +32,17 @@ export default function Sidebar() {
         <Navbar />
       </div>
 
-      <div></div>
+      <div className='w-[192px] py-[6px] px-3 rounded flex items-center gap-[10px] bg-navbar-200'>
+        <div className='flex items-center justify-center p-[6px] rounded bg-navbar-100'>
+          <WalletIcon />
+        </div>
+        <div className='flex flex-col gap-[2px]'>
+          <span className='text-[13px] leading-4 opacity-80'>
+            Available credits
+          </span>
+          <span className='text-base font-medium leading-6'>222.10</span>
+        </div>
+      </div>
     </div>
   );
 }
